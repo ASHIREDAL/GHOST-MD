3631e3764b1cabf9e015918bd32cbe844e3cc04a
ASHIREDAL <151848612+ASHIREDAL@users.noreply.github.com>
2024-10-27 12:33:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "xydhjI7B#vUsJ6Sslz4H17MlMw-snAAA-FHqR6OGuX-NMqP_aEyY",
+SESSION_ID: process.env.SESSION_ID || "ZA52nBhL#qdoUOtz7l9YQv4K_6LiF5Ot8LmC9wmjd9sH3cT3TTOo",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - somalian-breed\n\n*💻 Owner Number* -254729984157",
